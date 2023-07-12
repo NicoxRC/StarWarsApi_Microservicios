1acd64ca2b6fa319f9acc11cf45d6fd9d27d9012
NicoxRC <nicorojas005@gmail.com>
2023-07-12 12:37:39 -0500

    adding port for railway

--- database/src/controller/get.js
+++ database/src/controller/get.js
@@ -1,10 +1,10 @@
-// const store = require('../connection');
+const store = require('../connection');
 const Character = require('../connection/index');
 
 module.exports = async (req, res) => {
   try {
     const { model, id } = req.params;
-    const response = await Character.findById(id);
+    const response = await Character.get(id);
     res.status(200).json(response);
   } catch (error) {
     console.log({ error: error.message });
